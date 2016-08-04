1b9b0e7fef1d8bb1e4fa47a0f98adce2657fac2f
Andrew Chen <chenan02@gmail.com>
2016-08-03 19:25:01 -0500

    Add User name storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    User.create(
+      name: params[:user][:name]
+    )
+    redirect_to new_user_path
   end
 end
